524897ab4a11ac7d17eae7be9177f3a75197e94b
username <wecp-dummy@gmail.com>
2024-02-17 06:09:19 +0000

    Day23 Commit

--- frontend/src/app/auth/components/user/user.component.ts
+++ frontend/src/app/auth/components/user/user.component.ts
@@ -7,7 +7,7 @@ import { Customer } from "../../../bank/types/Customer"
 @Component({
   selector: "app-user",
   templateUrl: "./user.component.html",
-  styleUrls: ["./user.component.scss"],
+  styleUrls: ["./user.component.css"],
 })
 export class UserComponent implements OnInit {
   userForm: FormGroup;
